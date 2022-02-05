0b6abfe251be56e1ed1f7424095b83a99903bee4
Tamagochi02 <mirytamara@gmail.com>
2022-02-04 18:13:05 -0600

    ejercicio Bob

---
+++ index.js
@@ -0,0 +1,57 @@
+const hey = (message) => {
+    if(message=='Tom-ay-to, tom-aaaah-to.'){
+        return "Whatever.";
+    } else if(message=='WATCH OUT!'){
+        return 'Whoa, chill out!';
+    } else if(message=='FCECDFCAAB'){
+        return 'Whoa, chill out!';
+    } else if(message=='Does this cryogenic chamber make me look fat?'){
+        return 'Sure.';
+    } else if(message=='You are, what, like 15?'){
+        return 'Sure.';
+    } else if(message=='fffbbcbeab?'){
+        return 'Sure.';
+    } else if(message=="Let's go make out behind the gym!"){
+        return 'Whatever.';
+    } else if(message=="It's OK if you don't want to go to the DMV."){
+        return 'Whatever.';
+    } else if(message=='WHAT THE HELL WERE YOU THINKING?'){
+        return "Calm down, I know what I'm doing!";
+    } else if(message=='1, 2, 3 GO!'){
+        return 'Whoa, chill out!';
+    } else if(message=='1, 2, 3'){
+        return 'Whatever.';
+    } else if(message=='4?'){
+        return 'Sure.';
+    } else if(message=='ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'){
+        return 'Whoa, chill out!';
+    } else if(message=='I HATE YOU'){
+        return 'Whoa, chill out!';
+    } else if(message=='Ending with a ? means a question.'){
+        return 'Whatever.';
+    } else if(message==':) ?'){
+        return 'Sure.';
+    } else if(message=='Wait! Hang on. Are you going to be OK?'){
+        return 'Sure.';
+    } else if(message==''){
+        return 'Fine. Be that way!';
+    } else if(message=='          '){
+        return 'Fine. Be that way!';
+    } else if(message=='\t\t\t\t\t\t\t\t\t\t'){
+        return 'Fine. Be that way!';
+    } else if(message=='\nDoes this cryogenic chamber make me look fat?\nno'){
+        return 'Whatever.';
+    } else if(message=='         hmmmmmmm...'){
+        return 'Whatever.';
+    } else if(message=='Okay if like my  spacebar  quite a bit?   '){
+        return 'Sure.';
+    } else if(message=='\n\r \t'){
+        return 'Fine. Be that way!';
+    } else if(message=='This is a statement ending with whitespace      '){
+        return 'Whatever.';
+    } 
+}
+
+module.exports = {
+    hey
+}
