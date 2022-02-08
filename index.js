822d60af135e8143d513811a66a874d561919f22
Tamagochi02 <mirytamara@gmail.com>
2022-02-08 08:55:01 -0600

    Fix refactorizar la evaluacion de los test

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
 const hey = (message) => {
-    if(message=='Tom-ay-to, tom-aaaah-to.'){
-        return "Whatever.";
-    } else if(message=='WATCH OUT!'){
-        return 'Whoa, chill out!';
-    } else if(message=='FCECDFCAAB'){
-        return 'Whoa, chill out!';
-    } else if(message=='Does this cryogenic chamber make me look fat?'){
-        return 'Sure.';
-    } else if(message=='You are, what, like 15?'){
-        return 'Sure.';
-    } else if(message=='fffbbcbeab?'){
-        return 'Sure.';
-    } else if(message=="Let's go make out behind the gym!"){
-        return 'Whatever.';
-    } else if(message=="It's OK if you don't want to go to the DMV."){
-        return 'Whatever.';
-    } else if(message=='WHAT THE HELL WERE YOU THINKING?'){
+    message = message.trim()
+    if(message.toUpperCase()==message && message[message.length-1]=="?" && /[a-zA-Z]/g.test(message))
         return "Calm down, I know what I'm doing!";
-    } else if(message=='1, 2, 3 GO!'){
+
+    if(message.toUpperCase()==message && /[a-zA-Z]/g.test(message))
         return 'Whoa, chill out!';
-    } else if(message=='1, 2, 3'){
-        return 'Whatever.';
-    } else if(message=='4?'){
+
+    if(message[message.length-1]=="?" )
         return 'Sure.';
-    } else if(message=='ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'){
-        return 'Whoa, chill out!';
-    } else if(message=='I HATE YOU'){
-        return 'Whoa, chill out!';
-    } else if(message=='Ending with a ? means a question.'){
-        return 'Whatever.';
-    } else if(message==':) ?'){
-        return 'Sure.';
-    } else if(message=='Wait! Hang on. Are you going to be OK?'){
-        return 'Sure.';
-    } else if(message==''){
+
+    if(message=="")
         return 'Fine. Be that way!';
-    } else if(message=='          '){
-        return 'Fine. Be that way!';
-    } else if(message=='\t\t\t\t\t\t\t\t\t\t'){
-        return 'Fine. Be that way!';
-    } else if(message=='\nDoes this cryogenic chamber make me look fat?\nno'){
-        return 'Whatever.';
-    } else if(message=='         hmmmmmmm...'){
-        return 'Whatever.';
-    } else if(message=='Okay if like my  spacebar  quite a bit?   '){
-        return 'Sure.';
-    } else if(message=='\n\r \t'){
-        return 'Fine. Be that way!';
-    } else if(message=='This is a statement ending with whitespace      '){
-        return 'Whatever.';
-    } 
+
+    return 'Whatever.';
 }
 
 module.exports = {
